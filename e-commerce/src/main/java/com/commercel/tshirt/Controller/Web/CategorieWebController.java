c52009de72e0a60c5e83c6d1cfbfea1288e06deb
Nassim2389 <Nassim2389@hotmail.com>
2025-06-06 00:25:50 +0300

    add js on categorie page

--- e-commerce/src/main/java/com/commercel/tshirt/Controller/Web/CategorieWebController.java
+++ e-commerce/src/main/java/com/commercel/tshirt/Controller/Web/CategorieWebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class CategorieWebController {
 
-    @GetMapping("/")
+    @GetMapping("/categorie")
     public String cotegoriePage() {
         return "categorie";
     }
