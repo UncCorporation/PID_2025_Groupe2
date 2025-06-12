8916196c2534cd25e86bab8ab694898fcbe643bf
Sam <117534176+Panazu@users.noreply.github.com>
2025-06-12 15:55:45 +0200

    correctiion grosse erreur de nesting de fichiers

--- src/main/java/com/commercel/tshirt/Entity/Taille.java
+++
@@ -1,6 +0,0 @@
-package com.commercel.tshirt.Entity;
-
-public enum Taille {
-    XS, S, M, L, XL, XXL,
-    TAILLE_36, TAILLE_38, TAILLE_40, TAILLE_42, TAILLE_44
-}
--- src/main/java/com/commercel/tshirt/Repository/CategorieRepository.java
+++
@@ -1,6 +0,0 @@
-package com.commercel.tshirt.Repository;
-import com.commercel.tshirt.Entity.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieRepository extends JpaRepository<Categorie,Integer>{
-}
